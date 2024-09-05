57a2c5674ba2e949c240d279f0aeab19f991ffea
Daniel Trejo <dwtrejo14@comcast.net>
2024-09-05 09:09:25 -0400

    Challenge Complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api 
+from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound
 
 from models import db, Episode, Guest, Appearance
 
@@ -13,6 +14,124 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+
+
+
+class AllEpisodes(Resource):
+    def get(self):
+        try:
+            episodes_dict = [e.to_dict(only=('id', 'date', 'number')) for e  in Episode.query.all()]
+            response = make_response(episodes_dict, 200,)
+            return response
+        except:
+            response = make_response({'error': f'Episode {id} not found.'}, 404)
+            return response
+api.add_resource(AllEpisodes, '/episodes')
+
+
+
+
+
+class EpisodeInfo(Resource):
+    def get(self, id):
+        episode = Episode.query.get(id)
+        if episode:
+            guests = [
+                {
+                    "id": guest.id,
+                    "name": guest.name,
+                    "occupation": guest.occupation
+                }
+                for guest in episode.guests
+                ]
+            episode_dict = {
+                "id": episode.id,
+                "date": episode.date,
+                "number": episode.number,
+                "guests": guests
+            }
+            return make_response(episode_dict, 200)
+        else:
+            return make_response({"error": "404: Episode not found"}, 404)
+
+api.add_resource(EpisodeInfo, '/episodes/<int:id>')
+
+
+
+
+
+
+
+class OneEpisode(Resource):
+    def delete(self, id):
+        q = Episode.query.filter_by(id=id).first()
+        if(not q):
+            return make_response({'error': '404: Episode not found'}, 404)
+        db.session.delete(q)
+        db.session.commit()
+        return make_response({}, 204)
+api.add_resource(OneEpisode, '/episodes/<int:id>')
+
+
+
+
+
+
+
+class AllGuests(Resource):
+    def get(self):
+        guests = Guest.query.all()
+        docs_list = [g.to_dict(only=('id', 'name', 'occupation')) for g in guests]
+        return make_response(docs_list, 200)
+
+api.add_resource(AllGuests, '/guests')
+
+
+
+
+class CreateAppearance(Resource):
+    def post(self):
+
+        data = request.get_json()
+
+        rating = data.get('rating')
+        episode_id = data.get('episode_id')
+        guest_id = data.get('guest_id')
+
+        if not (1 <= rating <= 5):
+            raise ValueError('Raing must be between 1 and 5')
+
+        episode = Episode.query.get(episode_id)
+        guest = Guest.query.get(guest_id)
+
+        if episode and guest:
+            new_app = Appearance(rating=rating, episode_id=episode_id, guest_id=guest_id)
+            db.session.add(new_app)
+            db.session.commit()
+            appearance_dict = {
+                "id": new_app.id,
+                "rating": new_app.rating,
+                "episode_id": episode.id,
+                "guest_id": guest.id,
+                "episode": {
+                    "id": episode.id,
+                    "date": episode.date,
+                    "number": episode.number
+                },
+                "guest": {
+                    "id": guest.id,
+                    "name": guest.name,
+                    "occupation": guest.occupation
+                }
+            }
+            return make_response(appearance_dict, 201)
+        else:
+            return make_response({"error": "400: Validation error."}, 400)
+
+
+api.add_resource(CreateAppearance, '/appearances')
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -13,4 +13,73 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# add any models you may need. 
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', back_populates='episode')
+    guests = association_proxy('appearances', 'guest')
+
+    serialize_rules = ('-appearances.episode',)
+
+    def __repr__(self):
+        return f'<Episode {self.id} {self.date} {self.number}>'
+
+
+
+
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    episode = db.relationship('Episode', back_populates='appearances')
+    guest = db.relationship('Guest', back_populates='appearances')
+
+    @validates('rating')
+    def validate_rating(self, key, app_rating):
+        if not (1 <= app_rating <= 5):
+            raise ValueError('400: Validation error.')
+        return app_rating
+
+    serialize_rules = ('-episode.appearances', '-guest.appearances')
+
+    def __repr__(self):
+        return f'<Appearance {self.id} {self.rating} {self.episode_id} {self.guest_id} >'
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation =  db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', back_populates='guest')
+    episodes = association_proxy('appearances', 'episode')
+
+    
+    
+    
+    
+    serialize_rules=('-appearances.guest',)
+
+    
+    
+    
+    
+    def __repr__(self):
+        return f'<Guest {self.id} {self.name} {self.occupation}>'
